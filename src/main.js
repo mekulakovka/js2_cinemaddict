f0c2e8b5942d4c303b52ae04e6983ed560e2c8f5
kulakovka <kulakovka@gmail.com>
2024-09-21 18:06:30 +0400

    2.1 Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,18 +1,35 @@
 import HeaderProfileView from './view/header-profile-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
 import FilterView from './view/filter-view.js';
-import {render} from './render.js';
+
 import FilmsPresenter from './presenter/films-presenter.js';
+import FilmsModel from './model/films-model.js';
+import CommentsModel from './model/comments-model.js';
+
+import {render} from './render.js';
+
+const filmsModel = new FilmsModel();
+const commentsModel = new CommentsModel(filmsModel);
+
+//проверка работоспособности модулей
+/*
+import {generateFilms} from './mock/film';
+import {generateComments} from './mock/comment';
+const films = generateFilms();
+const comments = generateComments(films);
+console.log(films);
+console.log(comments); */
 
 const bodyElement = document.querySelector('body');
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const siteFooterElement = document.querySelector('.footer');
 const siteFooterStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
+
 const filmsPresenter = new FilmsPresenter();
 
 render(new HeaderProfileView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
 render(new FooterStatisticsView(), siteFooterStatisticsElement);
 
-filmsPresenter.init(siteMainElement);
+filmsPresenter.init(siteMainElement, filmsModel, commentsModel);
